7cf2c3cfbda52b65def59880909843d8dccbb3c1
ALFONSO VASQUEZ LUDEÑA <94215451+ALFONSOUTP@users.noreply.github.com>
2023-06-20 01:36:04 -0500

    Cambio del nombre del objeto input por teclado

---
+++ src/practicacalificada2/Pregunta4.java
@@ -0,0 +1,34 @@
+package practicacalificada2;
+import java.util.Scanner;
+public class Pregunta4 {
+	public static void main(String[] args) {
+	    Scanner input = new Scanner(System.in);
+	    System.out.print("Ingrese el valor de n: ");
+	    int n = input.nextInt();
+	    int[] primes = new int[n];
+	    int count = 0;
+	    int num = 2;
+	    while (count < n) {
+	      boolean isPrime = true;
+	      for (int i = 2; i <= Math.sqrt(num); i++) {
+	        if (num % i == 0) {
+	          isPrime = false;
+	          break;
+	        }
+	      }
+	      if (isPrime) {
+	        primes[count] = num;
+	        count++;
+	      }
+	      num++;
+	    }
+	    System.out.println("Los primeros " + n + " números primos son:");
+	    for (int prime : primes) {
+	      System.out.print(prime + " ");
+	    }
+	  }
+	}
+	
+	  
+
+
